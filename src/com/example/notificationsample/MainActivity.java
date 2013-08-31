f335d5b81edc750f54ab4559ce1a9069cac05a25
naominix <secure@Hisashi-no-MacBook-Pro.local>
2013-08-31 21:50:19 +0900

    Add Notification method.

--- src/com/example/notificationsample/MainActivity.java
+++ src/com/example/notificationsample/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.notificationsample;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
 
 public class MainActivity extends Activity
 {
@@ -11,5 +15,18 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public void fireNotification(View view) {
+    	Notification notification = new NotificationCompat.Builder(this)
+    	.setContentTitle("タイトル")
+    	.setContentText("わんわん！")
+    	.setTicker("一瞬出るヤツ")
+    	.setAutoCancel(true)
+    	.setSmallIcon(android.R.drawable.ic_dialog_info)
+    	.build();
+    	
+    	NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    	manager.notify(1,notification);
     }
 }
